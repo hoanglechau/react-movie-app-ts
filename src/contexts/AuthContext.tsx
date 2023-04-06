e31d19fc40920396abe38f4c4e9c05d0cd6cc33f
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-06 20:45:36 +0700

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,27 +1,37 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import { ReactNode, createContext, useContext, useMemo, useState } from "react";
 
 // This is the context that will be used to store the user data
 const AuthContextType = {
   user: "",
-  signin: null,
-  signout: null
+  signin: (a: string, b: () => void) => {},
+  signout: () => {}
 };
 const AuthContext = createContext(AuthContextType);
 
+interface Props {
+  children: ReactNode;
+}
+
+type value = {
+  user: string;
+  signin: (a: string, b: () => void) => void;
+  signout: () => void;
+};
+
 // This component is used to provide the context to the children
-export function AuthProvider({ children }) {
+export function AuthProvider({ children }: Props) {
   const [user, setUser] = useState("");
 
-  const signin = (newUser, callback) => {
+  const signin = (newUser: string, callback: () => void): void => {
     setUser(newUser);
     callback();
   };
 
-  const signout = () => {
-    setUser(null);
+  const signout = (): void => {
+    setUser("");
   };
 
-  const value = useMemo(() => ({ user, signin, signout }), [user]);
+  const value: value = useMemo(() => ({ user, signin, signout }), [user]);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
