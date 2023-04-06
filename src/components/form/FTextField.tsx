a8ab557b1fccdb89743a9b1ccb527879b9b3f17d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-06 07:02:51 +0700

    Create FTextField.tsx

---
+++ src/components/form/FTextField.tsx
@@ -0,0 +1,33 @@
+import { TextField } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+
+interface Props {
+  name: string;
+  label: string;
+}
+
+function FTextField({ name, label, ...other }: Props) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          label={label}
+          variant="filled"
+          color="secondary"
+          size="small"
+          fullWidth
+          error={!!error}
+          helperText={error?.message}
+          {...field} // get user input
+          {...other}
+        />
+      )}
+    />
+  );
+}
+
+export default FTextField;
